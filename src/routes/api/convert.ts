8894ee211ff790d6f256d051db7a75c316056cf7
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-04 08:00:07 +0200

    Finish the Task

--- src/routes/api/convert.ts
+++ src/routes/api/convert.ts
@@ -1,9 +1,34 @@
+import { promises as fsPromises, existsSync } from 'fs';
 import { Router } from 'express';
+import csv from 'csvtojson';
 
 const router = Router();
 
-router.get('/convert', (req, res) => {
+const filePath = './users.csv';
+const outputPath = 'users.json';
+
+router.get('/convert', async (req, res) => {
   res.send(`<h1>File is Processing now...</h1>`);
+
+  try {
+    if (existsSync(outputPath)) {
+      console.log('File already exists');
+      return;
+    } else {
+      console.log('file not found');
+    }
+    const usersData = await csv().fromFile(filePath);
+    const newData = usersData.map(
+      (item: { first_name: string; last_name: string; phone: string }) => ({
+        first_name: item.first_name,
+        last_name: item.last_name,
+        phone: item.phone === '' ? 'missing data' : item.phone
+      })
+    );
+    fsPromises.writeFile(outputPath, JSON.stringify(newData));
+  } catch (error) {
+    console.log(`ERROR: ${error}`);
+  }
 });
 
 export default router;
